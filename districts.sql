36f8d919b8aec56a5e8782454b7bd558df7a0bf4
CybSam <42088593+cybsam@users.noreply.github.com>
2022-09-17 11:33:11 +0600

    update districts

--- districts.sql
+++ districts.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.7
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 12, 2014 at 06:30 PM
--- Server version: 5.5.29
--- PHP Version: 5.4.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
