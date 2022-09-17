1bfc1c067c08d4cb054d3cf0f2fd7b0b1e511577
CybSam <42088593+cybsam@users.noreply.github.com>
2022-09-17 11:28:39 +0600

    Create division.sql

---
+++ division.sql
@@ -0,0 +1,33 @@
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `bd_geocode`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `divisions`
+--
+
+CREATE TABLE `divisions` (
+  `id` int(2) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `bn_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `divisions`
+--
+
+INSERT INTO `divisions` (`id`, `name`, `bn_name`) VALUES
+(1, 'Barisal', 'বরিশাল'),
+(2, 'Chittagong', 'চট্টগ্রাম'),
+(3, 'Dhaka', 'ঢাকা'),
+(4, 'Khulna', 'খুলনা'),
+(5, 'Rajshahi', 'রাজশাহী'),
+(6, 'Rangpur', 'রংপুর'),
+(7, 'Sylhet', 'সিলেট');
